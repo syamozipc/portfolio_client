2d7c25995aa368b16a7ac1d9b980f4a8db0c884b
syamozipc <syamozipc@gmail.com>
2023-07-29 12:56:50 +0900

    apiドメインの変更を反映

--- src/constant/app.ts
+++ src/constant/app.ts
@@ -1,2 +1,2 @@
 // export const APP_URL = "http://localhost:8082/api";
-export const APP_URL = "https://web-app-api.syamozipc.xyz/api";
+export const APP_URL = "https://portfolio.syamozipc.xyz/api";
