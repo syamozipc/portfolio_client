0d8a5c9f7e4bdfd252f45bcfdcaa786f42e92840
syamozipc <syamozipc@gmail.com>
2024-07-07 23:11:37 +0900

    tsエラーを修正

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,9 +1,9 @@
-import { Task } from "../types/task";
+import type { Task } from '../types/task';
 
 type Props = {
   task: Task;
 };
 
 export default function Task(props: Props) {
-  return <div style={{ fontSize: "20px" }}>{props.task.title}</div>;
+  return <div style={{ fontSize: '20px' }}>{props.task.title}</div>;
 }
